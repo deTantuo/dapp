5e8a0c645c201c7e219f5ab91b16067c4cc140e4
David Tantuo <david.tantuo@openarc.net>
2022-09-15 14:27:49 -0400

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,9 +55,13 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+           
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
